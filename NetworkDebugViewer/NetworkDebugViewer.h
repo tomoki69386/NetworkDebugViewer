a22be058728aebce9266248433b8f409ff1964cc
tomoki_sun <tomoki69386@gmail.com>
2019-12-27 16:27:25 +0900

    Initial Commit

---
+++ NetworkDebugViewer/NetworkDebugViewer.h
@@ -0,0 +1,19 @@
+//
+//  NetworkDebugViewer.h
+//  NetworkDebugViewer
+//
+//  Created by 築山朋紀 on 2019/12/27.
+//  Copyright © 2019 tomoki_sun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkDebugViewer.
+FOUNDATION_EXPORT double NetworkDebugViewerVersionNumber;
+
+//! Project version string for NetworkDebugViewer.
+FOUNDATION_EXPORT const unsigned char NetworkDebugViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkDebugViewer/PublicHeader.h>
+
+
