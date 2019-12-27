84ce785105b6817bec0c5917639b7a9576ec3d28
tomoki_sun <tomoki69386@gmail.com>
2019-12-27 19:03:04 +0900

    debug

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -10,23 +10,49 @@ import UIKit
 import NetworkDebugViewer
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let url = URL(string: "https://api.github.com/users/tomoki69386")!
+        let request = URLRequest(url: url)
+        ApiClient.send(request) { result in
+            switch result {
+            case .success:
+                break
+            case let .failure(error):
+                print(error)
+            }
+        }
     }
     
     @IBAction private func tapped(_ sender: UIButton) {
-        let url = URL(string: "https://example.com/v1/posts")!
-        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: ["Content-type": "application/json"])
-        let request = URLRequest(url: url)
-        let data = """
-        {"name": "tomoki_sun","twitter": "https://twitter.com/tomoki_sun"}
-        """.data(using: .utf8)!
-
-        let networkDebug = NetworkDebug(response, request: request, data: data)
-        
-        let viewController = NetworkDebugListViewController(with: [networkDebug])
+        let viewController = NetworkDebugListViewController(with: DataStore.shared.networkDebugs)
         let navigationController = UINavigationController(rootViewController: viewController)
         present(navigationController, animated: true)
     }
+}
+
+struct ApiClient {
+    static func send(_ request: URLRequest, completion: @escaping(Result<Void, Error>) -> Void) {
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            switch (data, response as? HTTPURLResponse, error) {
+            case let (.some(data), .some(response), _):
+                let network = NetworkDebug(response, request: request, data: data)
+                DataStore.shared.networkDebugs.append(network)
+                completion(.success(()))
+            case let (_, _, .some(error)):
+                completion(.failure(error))
+            default:
+                fatalError()
+            }
+        }
+        task.resume()
+    }
+}
+
+class DataStore {
+    static let shared = DataStore()
+    private init() { }
+    var networkDebugs: [NetworkDebug] = []
 }
